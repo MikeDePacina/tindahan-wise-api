e6f0bec90b05425c1ae525816a598f4c33e495ec
MikeDePacina <mikedpacina@gmail.com>
2026-01-10 10:18:04 +0800

    added unit testing to CICD pipeline

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ import mountRoutes from './routes/index.js';
 const app = express();
 const port = 3002;
 
-// const pool = new Pool({
-//   host: process.env.POSTGRES_HOST,
-//   user: process.env.POSTGRES_USER,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: process.env.POSTGRES_DB,
-//   port: process.env.POSTGRES_PORT,
-//   idleTimeoutMillis: 30000,
-// });
-
-console.log("Testing SAST scanner");
-
 app.use(express.json())
 
 mountRoutes(app)
@@ -27,6 +16,10 @@ app.get('/health', (req, res) =>{
   res.status(200).json({status: 'ok'})
 })
 
-app.listen(port, () => {
-  return console.log(`transactions service is listening at http://localhost:${port}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    return console.log(`transactions service is listening at http://localhost:${port}`);
+  });
+}
+
+export default app;
---
+++ index.test.js
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import app from './index.js';
+
+
+describe('GET /', () => {
+  it('should return HTTP 200 "Hello World"', async () => {
+    const res = await request(app).get('/');  // simulate GET request
+    expect(res.statusCode).toBe(200);        // assert status code is 200 
+  });
+});
